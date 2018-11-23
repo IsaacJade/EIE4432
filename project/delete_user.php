57488acee38eea2c700ea79a2504b9b6c18793ca
Tim <virtimmy@hotmail.com>
2018-11-23 19:36:44 +0800

    Add files via upload

--- project/delete_user.php
+++ project/delete_user.php
@@ -2,12 +2,17 @@
 	session_start();
 	
 	$conn=mysqli_connect("localhost","root","","users");
-	if($conn->connection_error)
+	if(!$conn)
 	{
 		echo "Unable to connect database";
 	}
-	$query="";
-	
+	$query1="delete from users where username=\"".$_SESSION["username"]."\"";
+	$query2="delete from post where username=\"".$_SESSION["username"]."\"";
+	$query3="delete from relation where userone=\"".$_SESSION["username"]."\" or usertwo=\"".$_SESSION["username"]."\"";
+	mysqli_query($conn,$query1);
+	mysqli_query($conn,$query2);
+	mysqli_query($conn,$query3);
 	$conn->close();
 	session_destroy();
+	header("Location:login.html");
 ?>
